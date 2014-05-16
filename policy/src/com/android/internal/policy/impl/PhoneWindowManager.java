9c5eb253d44bd18ad12ad6b5d10049a8145720e9
invisiblek <dan.pasanen@gmail.com>
2014-05-15 21:34:12 -0500

    immersive mode: don't unconfirm a package, that's just dumb
    * This is very annoying for specialized apps
    * See here for a great explanation http://stackoverflow.com/a/20309721
    * The fact that it doesn't even notify the user until the NEXT time they
      run it makes it completely pointless in my opinion anyway.
    
    Change-Id: I423611efc07d467e1d81c66d58adcff331be8b38
    Signed-off-by: anerikg <anerikgames@gmail.com>
    
    Conflicts:
    	policy/src/com/android/internal/policy/impl/PhoneWindowManager.java

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -4800,10 +4800,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 }
                 result &= ~ACTION_PASS_TO_USER;
                 if (down) {
-                    if (mExpandedDesktopMode == 0) {
-                        mImmersiveModeConfirmation.onPowerKeyDown(isScreenOn, event.getDownTime(),
-                                isImmersiveMode(mLastSystemUiFlags));
-                    }
                     if (isScreenOn && !mPowerKeyTriggered
                             && (event.getFlags() & KeyEvent.FLAG_FALLBACK) == 0) {
                         mPowerKeyTriggered = true;
