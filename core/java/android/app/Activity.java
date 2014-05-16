d8dbdfcf304bb1733fc69b047cf64e4e43cc9777
Evan Anderson <evan@thelinuxman.us>
2014-05-16 07:04:11 -0300

    Dismiss IME when floating window is dismissed
    Previously the IME would be stuck on screen if the
    floating window was dismissed while it was showing.
    
    Original id from PA gerrit -> Change-Id: I8ca61b07ed89bae85eabd8db23f87b6f1f0b179f
    Signed-off-by: Evan Anderson <evan@thelinuxman.us>
    
    Change-Id: If38e65f61ba309d22306bf4b37c3c55630c006ed

--- core/java/android/app/Activity.java
+++ core/java/android/app/Activity.java
@@ -4290,6 +4290,7 @@ public class Activity extends ContextThemeWrapper
      * @hide
      */
     public void finishFloating() {
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
         mMainThread.performFinishFloating();
     }
 
